c49b546ada2c6c7207d75c95b665b04852bda2d1
Seance <siddharth.dhananjay@gmail.com>
2020-09-22 21:18:30 +1000

    added a rendereatform function which will be called in the eat cbutton click. fixed buildAdvancedresponse.

--- Assets/script.js
+++ Assets/script.js
@@ -60,171 +60,171 @@ function hourlyTempCheck(response) {
 
 // checks the uv for the time of the day
 function hourlyUvCheck(response) {
-    var uvIndex = response.uv;
-    var uvThreshold = 2;
+  var uvIndex = response.uv;
+  var uvThreshold = 2;
 
-    if (uvIndex >= uvThreshold) {
-        // display uv message - this is where functions can got to display to the user what to wear - PLACEHOLDER
-        console.log(`UV Index: ${uvIndex}`);
-        console.log("You will need sunprotection today, wear a hat or apply sunscreen regularly");
-    } else {
-        // else because we don't want to give false info, this is a disclaimer
-        console.log("UV is low but it is still advised to protect yourself from UV");
-    }
+  if (uvIndex >= uvThreshold) {
+    // display uv message - this is where functions can got to display to the user what to wear - PLACEHOLDER
+    console.log(`UV Index: ${uvIndex}`);
+    console.log("You will need sunprotection today, wear a hat or apply sunscreen regularly");
+  } else {
+    // else because we don't want to give false info, this is a disclaimer
+    console.log("UV is low but it is still advised to protect yourself from UV");
+  }
 }
 
 // checks the precipitation for the time of the day
 function hourlyRainCheck(response) {
-    var precip = response.precip;
-    var precipThreshold = 5;
+  var precip = response.precip;
+  var precipThreshold = 5;
 
-    if (precip > precipThreshold) {
-        // display to the user what to wear - this is where functions can go to display info to the user - PLACEHOLDER
-        console.log(`Precipitation: ${precip}mm`);
-        console.log("You will need a waterproof or an umbrella");
-    }
+  if (precip > precipThreshold) {
+    // display to the user what to wear - this is where functions can go to display info to the user - PLACEHOLDER
+    console.log(`Precipitation: ${precip}mm`);
+    console.log("You will need a waterproof or an umbrella");
+  }
 }
 
 // checks the wind speed for the given time of day
 function hourlyWindCheck(response) {
-    var windSpeed = response.wind_spd;
-    var windSpeedThreshold = 10;
+  var windSpeed = response.wind_spd;
+  var windSpeedThreshold = 10;
 
-    if (windSpeed > windSpeedThreshold) {
-        // display to the user what to wear - this is where functions can go to update the display - PLACEHOLDER
-        console.log(`Wind Speed: ${windSpeed}km/h`);
-        console.log("You will need wind protection");
-    }
+  if (windSpeed > windSpeedThreshold) {
+    // display to the user what to wear - this is where functions can go to update the display - PLACEHOLDER
+    console.log(`Wind Speed: ${windSpeed}km/h`);
+    console.log("You will need wind protection");
+  }
 }
 
 // retrieves and stores the url for the weather icon for the time of the day
 function hourlyDisplayIcon(response) {
-    var iconCode = response.weather.icon;
-    var iconUrl = `https://www.weatherbit.io/static/img/icons/${iconCode}.png`
-    console.log(`Icon: ${iconUrl}`);
+  var iconCode = response.weather.icon;
+  var iconUrl = `https://www.weatherbit.io/static/img/icons/${iconCode}.png`
+  console.log(`Icon: ${iconUrl}`);
 }
 
 // finds the minimum temperature in an array of temps provided by the api
 function findMinTemp(temps) {
 
-    var minTemp = temps[0];
-    temps.forEach(function(temp) {
+  var minTemp = temps[0];
+  temps.forEach(function (temp) {
 
-        if (minTemp > temp) {
-            minTemp = temp;
-        }
-    });
-    console.log(`Min Temp: ${minTemp}`);
-    return minTemp;
+    if (minTemp > temp) {
+      minTemp = temp;
+    }
+  });
+  console.log(`Min Temp: ${minTemp}`);
+  return minTemp;
 }
 
 // finds the maximum temperature in an array of temps provided by the api
 function findMaxTemp(temps) {
-    var maxTemp = temps[0];
-    temps.forEach(function(temp) {
+  var maxTemp = temps[0];
+  temps.forEach(function (temp) {
 
-        if (maxTemp < temp) {
-            maxTemp = temp;
-        }
-    });
-    console.log(`Max Temp: ${maxTemp}`);
-    return maxTemp;
+    if (maxTemp < temp) {
+      maxTemp = temp;
+    }
+  });
+  console.log(`Max Temp: ${maxTemp}`);
+  return maxTemp;
 }
 
 // finds the average temperature in an array of temperatures provided by the weather api
 function findAverageTemp(temps) {
-    var avgTemp = 0;
-    var sum = 0;
-    temps.forEach(function(temp) {
-        sum += temp;
-    });
-    avgTemp = sum / temps.length;
-    console.log(`Avg Temp: ${avgTemp.toFixed(2)}`);
-    return avgTemp;
+  var avgTemp = 0;
+  var sum = 0;
+  temps.forEach(function (temp) {
+    sum += temp;
+  });
+  avgTemp = sum / temps.length;
+  console.log(`Avg Temp: ${avgTemp.toFixed(2)}`);
+  return avgTemp;
 }
 
 // processes the weather data retrieved from the weather api
 function processHourlyWeatherData(response) {
 
-    // stores response object in currentWeather object
-    console.log(response);
-    currentWeather.response = response;
+  // stores response object in currentWeather object
+  console.log(response);
+  currentWeather.response = response;
 
-    // store, retrieve in currentWeather object and console log the location and country
-    currentWeather.cityName = response.city_name;
-    currentWeather.countryCode = response.country_code
+  // store, retrieve in currentWeather object and console log the location and country
+  currentWeather.cityName = response.city_name;
+  currentWeather.countryCode = response.country_code
 
-    console.log(`Location: ${currentWeather.cityName}`);
-    console.log(`Country: ${currentWeather.countryCode}`);
+  console.log(`Location: ${currentWeather.cityName}`);
+  console.log(`Country: ${currentWeather.countryCode}`);
 
-    // retrieve and store the latitude and longitude of the responded weather data
-    // to be used for zomato api
-    currentWeather.latitude = getLatitude(response);
-    currentWeather.longitude = getLongitude(response);
+  // retrieve and store the latitude and longitude of the responded weather data
+  // to be used for zomato api
+  currentWeather.latitude = getLatitude(response);
+  currentWeather.longitude = getLongitude(response);
 
-    console.log(`Latitude: ${currentWeather.latitude}`);
-    console.log(`Longitude: ${currentWeather.longitude}`);
+  console.log(`Latitude: ${currentWeather.latitude}`);
+  console.log(`Longitude: ${currentWeather.longitude}`);
 
-    // grab the start and finish times from the user input
-    currentWeather.startTime = getStartTime();
-    currentWeather.finishTime = getFinishTime();
+  // grab the start and finish times from the user input
+  currentWeather.startTime = getStartTime();
+  currentWeather.finishTime = getFinishTime();
 
-    console.log(`Start Time: ${currentWeather.startTime}`);
-    console.log(`Finish Time: ${currentWeather.finishTime}`);
+  console.log(`Start Time: ${currentWeather.startTime}`);
+  console.log(`Finish Time: ${currentWeather.finishTime}`);
 
-    // array to store the temps for every hour
-    currentWeather.temps = [];
+  // array to store the temps for every hour
+  currentWeather.temps = [];
 
-    // go through every hour for the next 48 hours and display the data in the console
-    response.data.forEach(function(dataObject) {
+  // go through every hour for the next 48 hours and display the data in the console
+  response.data.forEach(function (dataObject) {
 
-        // retrieve and store 
-        var time = moment(dataObject.timestamp_local);
+    // retrieve and store 
+    var time = moment(dataObject.timestamp_local);
 
-        // if data index is after start time and before finish time then display data
-        if (moment(time).isSame(moment(currentWeather.startTime)) ||
-            moment(time).isAfter(moment(currentWeather.startTime)) && moment(time).isBefore(moment(currentWeather.finishTime)) ||
-            moment(time).isSame(moment(currentWeather.finishTime))) {
+    // if data index is after start time and before finish time then display data
+    if (moment(time).isSame(moment(currentWeather.startTime)) ||
+      moment(time).isAfter(moment(currentWeather.startTime)) && moment(time).isBefore(moment(currentWeather.finishTime)) ||
+      moment(time).isSame(moment(currentWeather.finishTime))) {
 
-            console.log(`------------------------------`);
-            console.log(`Time: ${moment(dataObject.timestamp_local).format("MMM Do, k:mm")}`);
-            console.log(`Weather Description: ${dataObject.weather.description}`);
-            hourlyTempCheck(dataObject);
-            currentWeather.temps.push(dataObject.temp);
-            hourlyUvCheck(dataObject);
-            hourlyRainCheck(dataObject);
-            hourlyWindCheck(dataObject);
-            hourlyDisplayIcon(dataObject);
-            console.log(`------------------------------`);
-        }
-    });
+      console.log(`------------------------------`);
+      console.log(`Time: ${moment(dataObject.timestamp_local).format("MMM Do, k:mm")}`);
+      console.log(`Weather Description: ${dataObject.weather.description}`);
+      hourlyTempCheck(dataObject);
+      currentWeather.temps.push(dataObject.temp);
+      hourlyUvCheck(dataObject);
+      hourlyRainCheck(dataObject);
+      hourlyWindCheck(dataObject);
+      hourlyDisplayIcon(dataObject);
+      console.log(`------------------------------`);
+    }
+  });
 
-    findAverageTemp(currentWeather.temps);
-    renderChosenWears();
+  findAverageTemp(currentWeather.temps);
+  renderChosenWears();
 }
 
 // creates a url friendly location using the users inputs to be added to the query URL
 function createLocation(cityInput, countryInput) {
-    var location = "";
+  var location = "";
 
-    if (cityInput != "" && countryInput === "") {
-        location += cityInput;
-    } else if (cityInput != "" && countryInput != "") {
-        location = `${cityInput},${countryInput}`;
-    }
-    return location;
+  if (cityInput != "" && countryInput === "") {
+    location += cityInput;
+  } else if (cityInput != "" && countryInput != "") {
+    location = `${cityInput},${countryInput}`;
+  }
+  return location;
 }
 
 // creates the url to retrieve weather data from the weather api
 // location input will be provided by user
 function createHourlyWeatherUrl(cityInput, countryInput) {
-    var location = createLocation(cityInput, countryInput);
-    var apiKey = getWeatherApiKey();
-    var baseUrl = "https://api.weatherbit.io/v2.0/forecast/hourly";
-    var queryUrl = baseUrl +
-        "?key=" + apiKey +
-        "&city=" + location;
-    return queryUrl;
+  var location = createLocation(cityInput, countryInput);
+  var apiKey = getWeatherApiKey();
+  var baseUrl = "https://api.weatherbit.io/v2.0/forecast/hourly";
+  var queryUrl = baseUrl +
+    "?key=" + apiKey +
+    "&city=" + location;
+  return queryUrl;
 }
 
 // returns location with lower case letters and replaces all whitespaces with url friendly %20
@@ -238,10 +238,10 @@ function getCountryInput() {
   // DAN!! - Here's a function to take the country from the user
   // Suggestion: place the Jquery for the country input here and grab the country input value and put it into the country variable below
 
-    // placeholder variable -- here is where to retrieve the user input data for country from the UI - PLACEHOLDER
-    var country = $("#country").val();
-    var countryFriendlyUrl = makeUrlFriendly(country);
-    return countryFriendlyUrl;
+  // placeholder variable -- here is where to retrieve the user input data for country from the UI - PLACEHOLDER
+  var country = $("#country").val();
+  var countryFriendlyUrl = makeUrlFriendly(country);
+  return countryFriendlyUrl;
 }
 
 // placeholder function to retrieve the city input from the user - PLACEHOLDER
@@ -250,33 +250,33 @@ function getCityInput() {
   // DAN!! - Here's the function to take the city from the user input
   // Suggestion: place the Jquery for the location input here and grab the city input value and put it into the city variable below
 
-    // placeholder value -- here is where to retrieve the user input data for city from the UI - PLACEHOLDER
-    var city = $("#location").val();
-    var cityFriendlyUrl = makeUrlFriendly(city);
-    return cityFriendlyUrl;
+  // placeholder value -- here is where to retrieve the user input data for city from the UI - PLACEHOLDER
+  var city = $("#location").val();
+  var cityFriendlyUrl = makeUrlFriendly(city);
+  return cityFriendlyUrl;
 }
 
 // calls the weather api for the hourly weather
 function callWeatherApi() {
-    var cityInput = getCityInput();
+  var cityInput = getCityInput();
 
-    // if the city input is empty then don't make the weather api call
-    if (cityInput != "") {
-        var countryInput = getCountryInput();
-        var queryUrl = createHourlyWeatherUrl(cityInput, countryInput);
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).
-        then(processHourlyWeatherData).
-        catch(function(error) {
-            console.log("Catch error: " + error.message);
-        });
-    } else {
-        // display error message if city is not inputted
-        $("#modal-message").text("city is empty");
-        $("#modal").show();
-    }
+  // if the city input is empty then don't make the weather api call
+  if (cityInput != "") {
+    var countryInput = getCountryInput();
+    var queryUrl = createHourlyWeatherUrl(cityInput, countryInput);
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).
+      then(processHourlyWeatherData).
+      catch(function (error) {
+        console.log("Catch error: " + error.message);
+      });
+  } else {
+    // display error message if city is not inputted
+    $("#modal-message").text("city is empty");
+    $("#modal").show();
+  }
 
 }
 
@@ -362,7 +362,7 @@ function renderChosenWears() {
 }
 
 function closeModal() {
-    $("#modal").hide();
+  $("#modal").hide();
 }
 
 // activates the call to the weather api
@@ -418,56 +418,89 @@ function DisplayResponse(obj) {
   var pn = obj.restaurants[0].restaurant.phone_numbers
   console.log(pn)
 }
-function gettingCuisineid(response) {
-  var cuisines = response.cuisines
-  var id = ""
-  cuisines.forEach(function (item) {
-    if ("Indian" == item.cuisine.cuisine_name) {
-      id = item.cuisine.cuisine_id
-    }
-  })
-  return id
-}
+// function gettingCuisineid(response) {
+//   var cuisines = response.cuisines
+//   var id = ""
+//   cuisines.forEach(function (item) {
+//     if ("Indian" == item.cuisine.cuisine_name) {
+//       id = item.cuisine.cuisine_id
+//     }
+//   })
+//   return id
+// }
 function buildAdvancedResponse(b) {
+  // Get the value of whatever option is selected from the drop down and store it in variable
+  var cuisineidval = ""
+  var sortval = ""
+  var radiusval = ""
+  var paramarray = [{ cuisines: cuisineidval }, { sort: sortval }, { radius: radiusval }]
+  var lat = currentWeather.latitude
+  var lon = currentWeather.longitude
+  var city = currentWeather.cityName
+  var baseURL = "https://developers.zomato.com/api/v2.1/search?"
+  var urlObj = {
+    entity_id: b,
+    q: city,
+    lat: lat,
+    lon: lon,
+    start: "0",
+    count: "5"
+  }
+  paramarray.forEach(function (item) {
+    Object.keys(item).forEach(function (key) {
+      if (item[key] !== "") { urlObj[key] = item[key] }
+    })
+  })
+  var buildURL = baseURL + $.param(urlObj)
   $.ajax({
-    url: "https://developers.zomato.com/api/v2.1/cuisines?city_id=" + b,
+    url: buildURL,
     method: "GET",
     headers: {
       "user-key": "19132a3a025302edc9b08eb44608d7c0",
       "content-type": "application/json"
     },
   }).then(function (response) {
-    var cuisineid = gettingCuisineid(response)
-    var paramarray = [{ cuisines: cuisineid }, { sort: "cost" }, { radius: "10M" }]
-    var lat = currentWeather.latitude
-    var lon = currentWeather.longitude
-    var city = currentWeather.cityName
-    var baseURL = "https://developers.zomato.com/api/v2.1/search?"
-    var urlObj = {
-      entity_id: b,
-      q: city,
-      lat: lat,
-      lon: lon,
-      start: "0",
-      count: "5"
-    }
-    paramarray.forEach(function (item) {
-      Object.keys(item).forEach(function (key) {
-        if (item[key] !== "") { urlObj[key] = item[key] }
-      })
-    })
-    var buildURL = baseURL + $.param(urlObj)
-    $.ajax({
-      url: buildURL,
-      method: "GET",
-      headers: {
-        "user-key": "19132a3a025302edc9b08eb44608d7c0",
-        "content-type": "application/json"
-      },
-    }).then(function (response) {
-      DisplayResponse(response)
-    })
+    DisplayResponse(response)
   })
+  // $.ajax({
+  //   url: "https://developers.zomato.com/api/v2.1/cuisines?city_id=" + b,
+  //   method: "GET",
+  //   headers: {
+  //     "user-key": "19132a3a025302edc9b08eb44608d7c0",
+  //     "content-type": "application/json"
+  //   },
+  // }).then(function (response) {
+  //   var cuisineid = gettingCuisineid(response)
+  //   var paramarray = [{ cuisines: cuisineid }, { sort: "cost" }, { radius: "10M" }]
+  //   var lat = currentWeather.latitude
+  //   var lon = currentWeather.longitude
+  //   var city = currentWeather.cityName
+  //   var baseURL = "https://developers.zomato.com/api/v2.1/search?"
+  //   var urlObj = {
+  //     entity_id: b,
+  //     q: city,
+  //     lat: lat,
+  //     lon: lon,
+  //     start: "0",
+  //     count: "5"
+  //   }
+  //   paramarray.forEach(function (item) {
+  //     Object.keys(item).forEach(function (key) {
+  //       if (item[key] !== "") { urlObj[key] = item[key] }
+  //     })
+  //   })
+  //   var buildURL = baseURL + $.param(urlObj)
+  //   $.ajax({
+  //     url: buildURL,
+  //     method: "GET",
+  //     headers: {
+  //       "user-key": "19132a3a025302edc9b08eb44608d7c0",
+  //       "content-type": "application/json"
+  //     },
+  //   }).then(function (response) {
+  //     DisplayResponse(response)
+  //   })
+  // })
 }
 function buildLocationIDUrl() {
   var lat = currentWeather.latitude
@@ -485,7 +518,23 @@ function buildLocationIDUrl() {
   var buildlocalURL = baseURL + $.param(urlObj)
   return buildlocalURL
 }
-
+function renderEatform(entityid) {
+  $("selector").removeClass("hide")
+  $.ajax({
+    url: "https://developers.zomato.com/api/v2.1/cuisines?city_id=" + entityid,
+    method: "GET",
+    headers: {
+      "user-key": "19132a3a025302edc9b08eb44608d7c0",
+      "content-type": "application/json"
+    },
+  }).then(function (response) {
+    var cuisines = response.cuisines
+    cuisines.forEach(function (item) {
+      var option = $("<option>").text(item.cuisine.cuisine_name).attr("data-id", item.cuisine.cuisine_id )
+      $("#cuisine").append(option)
+    })
+  })
+}
 $(".eat").on("click", function gettingEntityId() {
   $.ajax({
     url: buildLocationIDUrl(),
@@ -505,6 +554,7 @@ $(".eat").on("click", function gettingEntityId() {
         "content-type": "application/json"
       },
     }).then(function (response) {
+      renderEatform(entityid)
       DisplayResponse(response)
     })
   })
